6ac91e1714cbfbc68d5609267e35038673dc5c30
JessyLeeee <804138731@qq.com>
2026-01-18 20:32:44 +0800

    初始版本

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import PioneerList from '../views/PioneerList.vue'
+import RedBlackList from '../views/RedBlackList.vue'
+import QuotesList from '../views/QuotesList.vue'
+import StoriesList from '../views/StoriesList.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    { path: '/', component: HomeView },
+  { path: '/pioneer', component: PioneerList },
+  { path: '/redblack', component: RedBlackList },
+  { path: '/quotes', component: QuotesList },
+  { path: '/stories', component: StoriesList },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+    },
+  ],
+})
+
+export default router
