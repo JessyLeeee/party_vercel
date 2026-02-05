98566898f415a4a26b93c98e7f47f9baa6cde509
JessyLee <13066569+jessyleeee@user.noreply.gitee.com>
2026-02-05 23:49:58 +0800

    .

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import PioneerList from '../views/PioneerList.vue'
 import RedBlackList from '../views/RedBlackList.vue'
@@ -6,7 +6,7 @@ import QuotesList from '../views/QuotesList.vue'
 import StoriesList from '../views/StoriesList.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
